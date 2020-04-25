0a6ea886e94f96a27153ae7f88c6d5193fcc368a
Yu Shengyuan <1749238278@qq.com>
2020-04-25 11:41:55 +0800

    完成登陆

--- src/views/loginPage/login.vue
+++ src/views/loginPage/login.vue
@@ -10,7 +10,7 @@
     .login-con {
         width: 80%;
         margin-left: 10%;
-        background-color: white;
+        margin-top: 54%;
     }
 
     .username-con {
@@ -18,7 +18,6 @@
         width: 80%;
         margin-left: 10%;
         margin-bottom: 30px;
-        background-color: #00B4FF;
     }
 
     .username-con input {
@@ -29,21 +28,71 @@
         text-align: center;
         border: 0;
         outline: none;
+        font-size: 40px;
+        background-color: transparent;
+        border-bottom: 1px solid #f2e2e2;
+        color: white;
     }
+
+    .submit {
+        height: 100px;
+        width: 60%;
+        border-radius: 80px;
+        margin-left: 20%;
+        margin-top: 100px;
+        background-color: rgb(234,204,32);
+        line-height: 100px;
+        text-align: center;
+        font-size: 40px;
+        color: white;
+    }
+
+    .background-image {
+        position: absolute;
+        height: 100%;
+        width: 100%;
+    }
+
+    .app-logo {
+        position: absolute;
+        height: 200px;
+        width: 200px;
+        top: 10%;
+        border-radius: 50%;
+        background-color: #00B4FF;
+        left: calc(50% - 100px);
+    }
+
+    .register-notice {
+        position: absolute;
+        bottom: 100px;
+        height: 30px;
+        width: 100%;
+        text-align: center;
+        font-size: 40px;
+        color: #bcb0b0;
+    }
+
 
 
 </style>
 
 <template>
     <div class="register-login-con">
-       <div class="login-con">
+        <image class="background-image" src="http://printer.noerror.xyz/appImage/background.png"></image>
+        <div class="app-logo"></div>
+        <div class="login-con">
+            <image src="http://printer.noerror.xyz/appImage/background.png"></image>
             <div class="username-con">
+
                 <input type="text" placeholder="username">
             </div>
-           <div class="username-con">
-               <input type="text" placeholder="password">
-           </div>
-       </div>
+            <div class="username-con">
+                <input type="text" placeholder="password">
+            </div>
+        </div>
+        <div class="submit">Log in</div>
+        <div class="register-notice">New here?Sign Up</div>
     </div>
 </template>
 
